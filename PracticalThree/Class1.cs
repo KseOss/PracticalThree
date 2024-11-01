4e1665fca753ed5f286f37ddaac13b176debcdf2
АДМИН <@DESKTOP-S9U5IDE>
2024-11-01 22:54:46 +0300

    Добавьте файлы проекта.

---
+++ PracticalThree/Class1.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace LibMas
+{
+    public static class VisualArray
+    {
+        // Метод для одномерного массива
+        public static DataTable ToDataTable<T>(this T[] arr)
+        {
+            var res = new DataTable();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res.Columns.Add("col" + (i + 1), typeof(T));
+            }
+            var row = res.NewRow();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                row[i] = arr[i];
+            }
+            res.Rows.Add(row);
+            return res;
+        }
+
+        // Метод для двумерного массива
+        public static DataTable ToDataTable<T>(this T[,] matrix)
+        {
+            var res = new DataTable();
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                res.Columns.Add("col" + (i + 1), typeof(T));
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var row = res.NewRow();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    row[j] = matrix[i, j];
+                }
+                res.Rows.Add(row);
+            }
+
+            return res;
+        }
+        public static class MatrixOperations
+        {
+            // Метод для нахождения произведения элементов каждого столбца
+            public static double[] ProductOfColumns(double[,] matrix)
+            {
+                int rows = matrix.GetLength(0);
+                int cols = matrix.GetLength(1);
+                double[] products = new double[cols];
+
+                for (int j = 0; j < cols; j++)
+                {
+                    products[j] = 1; // Начальное значение для произведения
+                    for (int i = 0; i < rows; i++)
+                    {
+                        products[j] *= matrix[i, j];
+                    }
+                }
+
+                return products;
+            }
+        }
+    }
+}
---
+++ PracticalThree/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using LibMas;
+using System;
+using System.Data;
+using System.Windows;
+using System.Printing;
+using static LibMas.VisualArray;
+
+namespace PracticalThree
+{
+    public partial class MainWindow : Window
+    {
+        private double[,] matrix;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Обработчик кнопки "Выход"
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        // Обработчик кнопки "О программе"
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработчик: Сухомяткина Ксения\nНомер работы: 3\nЗадание: Для каждого столбца матрицы найти произведение его элементов.", "О программе");
+        }
+
+        // Обработчик кнопки для заполнения матрицы
+        private void FillMatrix_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(InputM.Text, out int m) && int.TryParse(InputN.Text, out int n) && m > 0 && n > 0)
+            {
+                matrix = new double[m, n];
+                Random random = new Random();
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrix[i, j] = random.Next(1, 10); // Заполнение случайными числами от 1 до 9
+                    }
+                }
+
+                // Преобразование в DataTable и вывод, если необходимо
+                DataTable dataTable = VisualArray.ToDataTable(matrix);
+                MessageBox.Show("Матрица заполнена.", "Уведомление");
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите положительные целые числа для M и N.", "Ошибка ввода");
+            }
+        }
+
+        // Обработчик кнопки для вычисления произведения столбцов
+        private void CalculateProduct_Click(object sender, RoutedEventArgs e)
+        {
+            if (matrix != null)
+            {
+                double[] products = MatrixOperations.ProductOfColumns(matrix);
+                ResultText.Text = "Произведения столбцов:\n" + string.Join("\n", products);
+            }
+            else
+            {
+                MessageBox.Show("Сначала заполните матрицу.", "Ошибка");
+            }
+        }
+    }
+}
